39c31c46c135043e5edf25d1776c5c882af5c97f
gerald <geraldbaalham@live.co.uk>
2024-10-15 15:11:32 +0100

    hero skeleton done

--- app/(front)/page.tsx
+++ app/(front)/page.tsx
@@ -1,7 +1,9 @@
+import Hero from "@/components/Frontend/Hero";
+
 export default function Home() {
   return (
-    <section className="text-3xl flex min-h-screen justify-center items-center">
-      <h2 className="font-semibold">Medical app</h2>
+    <section className="">
+      <Hero />
     </section>
   );
 }
